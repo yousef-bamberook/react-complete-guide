a7a553cbd74b778251edfcc1820589006deefc1b
yousef <abamby2007@gmail.com>
2023-04-01 18:38:50 +0300

    Working with lists

--- 03-more-practice-on-basics/src/App.js
+++ 03-more-practice-on-basics/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersLists';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, userAge) => {
+    setUsersList((pervUsersList) => {
+      return [
+        ...pervUsersList,
+        { id: Math.random().toString(), name: username, age: userAge },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- 03-more-practice-on-basics/src/components/Users/AddUser.js
+++ 03-more-practice-on-basics/src/components/Users/AddUser.js
@@ -10,7 +10,16 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -27,10 +36,16 @@ const AddUser = (props) => {
         <input
           id="username"
           type="text"
+          value={enteredUsername}
           onChange={usernameChangeHandler}
         ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit"> Add User</Button>
       </form>
     </Card>
