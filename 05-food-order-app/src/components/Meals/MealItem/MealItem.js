7b90ff7656486f7dffbec1c594f1158dd38ac0c0
yousef <abamby2007@gmail.com>
2023-04-02 08:08:37 +0300

    Build main components

---
+++ 05-food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+
+const MealItem = (props) => {
+  const price = `$${props.meal.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3 className={classes.name}>{props.meal.name}</h3>
+        <div className={classes.description}>{props.meal.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.meal.id} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
