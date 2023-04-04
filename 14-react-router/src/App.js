542dc3b5d647c097ecdaa820d579812d1293401b
yousef <abamby2007@gmail.com>
2023-04-04 08:26:48 +0300

    Defining routes with basics

--- 14-react-router/src/App.js
+++ 14-react-router/src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/ProductsPage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ 14-react-router/src/pages/ProductsPage.js
@@ -0,0 +1,4 @@
+const ProductsPage = () => {
+  return <h1>My Product Page</h1>;
+};
+export default ProductsPage;
