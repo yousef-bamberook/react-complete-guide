543987fd3c1906d899c52adf6656753875a5d392
yousef <abamby2007@gmail.com>
2023-04-04 08:44:52 +0300

    structure the main navigation bar and root

--- 14-react-router/src/App.js
+++ 14-react-router/src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/ProductsPage';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
