bdbcbf1ae18c319ecd64f7d867a435ed9160e51e
yousef <abamby2007@gmail.com>
2023-04-06 18:28:21 +0300

    Working with defer()

--- 15-react-router-project/frontend/src/pages/Events.js
+++ 15-react-router-project/frontend/src/pages/Events.js
@@ -1,27 +1,43 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-//loader of the page will be called right when we start navigating to
-//the page , NOT after the page component has been rendered
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const resDate = await response.json();
+    return resDate.events;
   }
+}
+
+//loader of the page will be called right when we start navigating to
+//the page , NOT after the page component has been rendered
+export function loader() {
+  /*
+    defer feature can speed up pages
+    and make sure that we're already showing some content
+    while waiting for other content.
+    It especially good if we have pages with
+    multiple HTTP requests with different speeds
+  */
+  return defer({
+    events: loadEvents(),
+  });
 }
