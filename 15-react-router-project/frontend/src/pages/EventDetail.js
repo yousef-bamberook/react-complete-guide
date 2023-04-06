733cca16286b26a514ece61743ee71b1a20c8f59
yousef <abamby2007@gmail.com>
2023-04-06 09:42:33 +0300

    Delete by submitting programmatically + disable submit btn on post

--- 15-react-router-project/frontend/src/pages/EventDetail.js
+++ 15-react-router-project/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from 'react-router-dom';
+import { json, redirect, useRouteLoaderData } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 
 const EventDetailsPage = () => {
@@ -21,5 +21,19 @@ export async function loader({ request, params }) {
     );
   } else {
     return response;
+  }
+}
+
+export async function action({ params, request }) {
+  const id = params.eventId;
+
+  const response = await fetch('http://localhost:8080/events/' + id, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: 'Could not delete event.' }, { status: 500 });
+  } else {
+    return redirect('/events');
   }
 }
